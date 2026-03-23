a7067bfd3e30c9d61e8d02fc27c68d36a2591e17
Mark Stealer Whitehall <marshall@oxasploits.com>
2026-03-22 22:13:13 -0400

    Delete .nvim.lua

--- .nvim.lua
+++
@@ -1,6 +0,0 @@
-vim.keymap.set(
-	"n",
-	"<leader>cP",
-	":botright 10split | term cloc Main/Frontend/src/*.js Main/Frontend/src/assets/scripts/*.js Main/Backend/*.py Main/Frontend/src/*.html Main/Frontend/src/*.js Main/Frontend/src/assets/scripts/scripts.js | grep Language -A 10000 -B 1<CR>",
-	{ desc = "LOC in the current project" }
-)
